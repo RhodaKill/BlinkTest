3064bda9c2015504dff992d7a14eb8813ec8f60a
Rhoda Kill <moonlightkinesis@gmail.com>
2025-02-08 09:49:40 +0000

    Initial template project

---
+++ BlinkTest/BlinkTestApp.swift
@@ -0,0 +1,15 @@
+//
+//  BlinkTestApp.swift
+//  BlinkTest
+//
+
+import SwiftUI
+
+@main
+struct BlinkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
